1082bac4ac5b83b8aab96ac0d0a0536711474a8b
hamadatomoki <hamada.tomoki01@gmail.com>
2023-05-05 14:07:27 +0900

    :books: learn: add randevous channel code

--- tutorials/intro-coroutines/src/tasks/Request7Channels.kt
+++ tutorials/intro-coroutines/src/tasks/Request7Channels.kt
@@ -1,9 +1,8 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 suspend fun loadContributorsChannels(
     service: GitHubService,
@@ -11,6 +10,24 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val channel = Channel<List<User>>()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+        for (repo in repos) {
+            launch {
+                val users = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+        var allUser = emptyList<User>()
+        repeat(repos.size) {
+            val users = channel.receive()
+            allUser  = (allUser + users).aggregate()
+            updateResults(allUser,it == repos.lastIndex)
+        }
     }
 }
